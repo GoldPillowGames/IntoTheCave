8de712222b0f588a370b8a920d4d3a7b14d392c2
Fernando Martín Espina <fernandomartin4@gmail.com>
2020-12-23 17:37:04 +0100

    Revert "Revert "Enemigo Melee sin animaciones""
    This reverts commit 4a9374761896a4bef89b48db2b912a200e153e8a.

---
+++ Rogue-Lite/Assets/Scripts/Patterns/EnemyState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace GoldPillowGames.Patterns
+{
+    public class EnemyState : State
+    {
+        #region Variables
+        protected string animatorBoolParameterName;
+        protected readonly Animator anim;
+        #endregion
+        
+        #region Methods
+        public EnemyState(FiniteStateMachine stateMachine, Animator anim) : base(stateMachine)
+        {
+            this.anim = anim;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            
+            anim.SetBool(animatorBoolParameterName, true);
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            
+            anim.SetBool(animatorBoolParameterName, false);
+        }
+        #endregion
+    }
+}
